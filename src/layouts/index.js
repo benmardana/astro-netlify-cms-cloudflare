49812b84b8445e61810bafa3c3d85458e87a1997
Ben McGarvey <b.mcgarvey.bm@gmail.com>
2022-11-04 01:56:28 +0000

    moves cms config to astro page

--- src/layouts/index.js
+++
@@ -1,3 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const Index = ({ title }) => (_jsx("header", { children: _jsx("h1", { children: _jsx("a", { className: "underlined", href: "#", children: title }) }) }));
-export default Index;
---
+++ src/pages/admin/cms.ts
@@ -0,0 +1,40 @@
+import CMS from 'netlify-cms-app';
+import Index from '../../layouts/index';
+
+CMS.init({
+  config: {
+    backend: {
+      name: 'github',
+      repo: 'benmcgarvey/promo',
+      branch: 'main',
+    },
+    publish_mode: 'editorial_workflow',
+    media_folder: 'public/assets',
+    site_url: 'https://promo-dfh.pages.dev',
+    collections: [
+      {
+        label: 'Pages',
+        name: 'pages',
+        files: [
+          {
+            label: 'Index',
+            name: 'index',
+            file: 'src/pages/index.md',
+            fields: [
+              { label: 'Title', name: 'title', widget: 'string' },
+              { label: 'Description', name: 'description', widget: 'string' },
+              { label: 'Layout', name: 'layout', widget: 'hidden' },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+});
+
+CMS.registerPreviewStyle('../../styles/global.css');
+CMS.registerPreviewTemplate('index', ({ entry }) => {
+  const props = entry.get('data').toJS();
+
+  return Index(props);
+});
